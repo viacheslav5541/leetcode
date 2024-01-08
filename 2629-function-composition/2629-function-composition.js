61511dea6097332d452768ad295572edf19a9ca0
viacheslav5541 <40827198+viacheslav5541@users.noreply.github.com>
2024-01-08 20:03:56 +0900

    Time: 71 ms (23.77%), Space: 43.3 MB (57.38%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+	return function(x) {
+      return  functions.reduceRight((acc,curr)=> {
+            return  curr(acc)
+        },x)
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
