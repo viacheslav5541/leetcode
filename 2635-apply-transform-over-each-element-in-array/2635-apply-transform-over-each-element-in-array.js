2f91aeb54ad89c1f50c1538163b22c10ea8f65cc
viacheslav5541 <40827198+viacheslav5541@users.noreply.github.com>
2024-01-08 20:05:25 +0900

    Time: 47 ms (83.51%), Space: 42.1 MB (31.43%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+  arr.forEach((item,index)=>arr[index]=fn(item,index))
+  return arr
+};
